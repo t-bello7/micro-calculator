ea56ede843f8885e63a89f5108ce7e451a319984
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-15 11:43:03 +0100

    Add some testing part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React from 'react';
 
-const Home = () => {
-    return (
-        <main className="container">
-            <h1>Welcome to our Page</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, unde animi possimus ullam explicabo repellendus soluta sint temporibus sed quisquam necessitatibus nemo pariatur quis a id officia. Sapiente, neque vero.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem aut reprehenderit beatae temporibus, minima impedit cumque maiores asperiores pariatur doloribus labore doloremque ullam dolor voluptatibus repudiandae ex accusamus nihil?</p>
-        </main>
-    )
+function Home() {
+  return (
+    <main className="container">
+      <h1>Welcome to our Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
+        unde animi possimus ullam explicabo repellendus soluta sint temporibus
+        sed quisquam necessitatibus nemo pariatur quis a id officia. Sapiente, neque vero.
+
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem
+        aut reprehenderit beatae temporibus, minima impedit cumque maiores asperiores
+        pariatur doloribus labore doloremque ullam dolor ?
+
+      </p>
+    </main>
+  );
 }
 
 export default Home;
